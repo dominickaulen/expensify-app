030c18e66d47958b73ef873a11d7cf18d40a948c
Dominic Kaulen <dominickaulen02@gmail.com>
2021-05-24 19:03:47 +0200

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
+const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
